05d928700efdcacef47c7b6dcbed2cdb4a99facb
tristyn stimpson <=>
2018-02-01 00:24:44 -0800

    Renamed FactorioTas folder to src

--- tasks/deploy.bat
+++ tasks/deploy.bat
@@ -1,2 +1,2 @@
 RD /S /Q "lib\factorio 0.15\mods\FactorioTas_0.0.5"
-xcopy /I /E /Y "FactorioTas" "lib\factorio_0.15\mods\FactorioTas_0.0.5"
+xcopy /I /E /Y "src" "lib\factorio_0.15\mods\FactorioTas_0.0.5"
