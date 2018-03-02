e3e6bc97a851e4598c644b1d2bcd048f30b50311
tristyn stimpson <=>
2018-03-02 13:13:57 -0800

    Made Event reentrant. it's totally broken ATM, ltrie is giving errors

--- src/Event.lua
+++ src/Event.lua
@@ -1,3 +1,7 @@
+local Map = require("ltrie.hashmap")
+
+-- An event object that supports reentrancy.
+
 local Event = { }
 local metatable = { __index = Event }
 
@@ -7,8 +11,7 @@ end
 
 function Event.new()
 	local new = {
-		callback_objects = { },
-		callback_objects_num_entries = { }
+		callback_objects = Map.of()
 	}
 
 	Event.set_metatable(new)
@@ -17,57 +20,65 @@ function Event.new()
 end
 
 function Event:add(callback_object, callback_function_name)
+	fail_if_missing(callback_object)
+	fail_if_missing(callback_function_name)
+
 	-- ensure the callback exists and is callable
-	local func = callback_object[callback_function_name]
-	if Event._is_callable(func) == false then
+	if Event._is_callable(callback_object, callback_function_name) == false then
 		error("Couldn't find function " .. callback_function_name .. " in callback object.")
 	end
 
-	local obj_callbacks = self.callback_objects[callback_object]
-	local obj_num_entries = self.callback_objects_num_entries[callback_object]
+	local obj_callbacks = self.callback_objects:get(callback_object)
 	if obj_callbacks == nil then
-		obj_callbacks = { }
-		self.callback_objects[callback_object] = obj_callbacks
-	end
-	if obj_num_entries == nil then
-		obj_num_entries = 0
-	end
-	
-	if obj_callbacks[callback_function_name] ~= nil then
+		obj_callbacks = Map.from( pairs({ [callback_function_name] = callback_function_name }))
+	elseif obj_callbacks:get(callback_function_name) ~= nil then
 		error("The callback was added twice.")
+	else
+		obj_callbacks = obj_callbacks:assoc(callback_function_name, callback_function_name)
 	end
 
-	obj_num_entries = obj_num_entries + 1
-	obj_callbacks[callback_function_name] = callback_function_name
-	self.callback_objects_num_entries[callback_object] = obj_num_entries
+	self.callback_objects = self.callback_objects:assoc(callback_object, obj_callbacks)
 end
 
 function Event:invoke(...)
-	for object, callback_names in pairs(self.callback_objects) do
-		for function_name, _ in pairs(callback_names) do
+	-- grab a local copy of the immutable structure in case it is modified
+	-- during the event invocation.
+	local callback_objects = self.callback_objects
+
+	for _it, object, callback_names in callback_objects:iter() do
+		for __it, function_name, _ in callback_names:iter() do
 			object[function_name](object, ...)
 		end
 	end
 end
 
 function Event:remove(callback_object, callback_function_name)
-	local obj_callbacks = self.callback_objects[callback_object]
-	if obj_callbacks == nil then error() end
-	local callback_exists = obj_callbacks[callback_function_name] ~= nil
+	fail_if_missing(callback_object)
+	fail_if_missing(callback_function_name)
 
-	if not callback_exists == true then error()	end
-	obj_callbacks[callback_function_name] = nil
+	local obj_callbacks = self.callback_objects:get(callback_object)
 	
-	self.callback_objects_num_entries[callback_object] = self.callback_objects_num_entries[callback_object] - 1
-	local num_entries = self.callback_objects_num_entries[callback_object]
-	if num_entries == 0 then
-		self.callback_objects[callback_object] = nil
-		self.callback_objects_num_entries[callback_object] = nil
+	if obj_callbacks == nil then 
+		error()
+	end
+	
+	if obj_callbacks:get(callback_function_name) == nil then
+		error()
+	end
+
+	obj_callbacks = obj_callbacks:dissoc(callback_function_name)
+
+	if obj_callbacks:len() > 0 then
+		self.callback_objects = self.callback_objects:assoc(callback_object, obj_callbacks)
+	else
+		self.callback_objects = self.callback_objects:dissoc(callback_object)
 	end
 
 end
 
-function Event._is_callable(func)
+function Event._is_callable(callback_object, callback_function_name)
+	local func = callback_object[callback_function_name]
+
 	if type(func) == "function" then
 		return true
 	end
