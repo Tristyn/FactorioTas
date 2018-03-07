1086489963474798ce862af58de1732282ae103f
tristyn stimpson <=>
2018-03-06 18:31:20 -0800

    Woops event should work

--- src/Event.lua
+++ src/Event.lua
@@ -1,5 +1,3 @@
-local Map = require("ltrie.hashmap")
-
 -- An event object that supports reentrancy.
 
 local Event = { }
@@ -22,6 +20,11 @@ function Event.new()
 end
 
 function Event:add(callback_object, callback_function_name)
+	-- ensure the callback exists and is callable
+	if Event._is_func_callable(callback_object, callback_function_name) == false then
+		error("Couldn't find function " .. callback_function_name .. " in callback object.")
+	end
+
 	if self:_is_call_reentrant() then
 		self:_add_during_reentrancy(callback_object, _callback_function_name)
 	else
@@ -30,36 +33,28 @@ function Event:add(callback_object, callback_function_name)
 end
 
 function Event:_add(callback_object, callback_function_name)
-	-- ensure the callback exists and is callable
-	local func = callback_object[callback_function_name]
-	if Event._is_func_callable(func) == false then
-		error("Couldn't find function " .. callback_function_name .. " in callback object.")
-	end
-
-	local obj_callbacks = self.callback_objects:get(callback_object)
+	local obj_callbacks = self.callback_objects[callback_object]
+	local obj_num_entries = self.callback_objects_num_entries[callback_object]
 	if obj_callbacks == nil then
-		obj_callbacks = Map.EMPTY:assoc(callback_function_name, callback_function_name)
-	elseif obj_callbacks:get(callback_function_name) ~= nil then
+		obj_callbacks = { }
+		self.callback_objects[callback_object] = obj_callbacks
+	end
+	if obj_num_entries == nil then
+		obj_num_entries = 0
+	end
+	
+	if obj_callbacks[callback_function_name] ~= nil then
 		error("The callback was added twice.")
-	else
-		obj_callbacks = obj_callbacks:assoc(callback_function_name, callback_function_name)
 	end
 
-	self.callback_objects = self.callback_objects:assoc(callback_object, obj_callbacks)
-
-	assert(self.callback_objects:get(callback_object):get(callback_function_name) == callback_function_name)
+	obj_num_entries = obj_num_entries + 1
+	obj_callbacks[callback_function_name] = callback_function_name
+	self.callback_objects_num_entries[callback_object] = obj_num_entries
 end
 
 function Event:_add_during_reentrancy(callback_object, callback_function_name)
 	-- to make the call reentrant we must treat self.callback_objects
 	-- as an immutable objects:
-
-
-	-- ensure the callback exists and is callable
-	local func = callback_object[callback_function_name]
-	if Event._is_func_callable(func) == false then
-		error("Couldn't find function " .. callback_function_name .. " in callback object.")
-	end
 
 	-- update self.callback_objects
 	local obj_callbacks = self.callback_objects[callback_object]
@@ -108,9 +103,9 @@ end
 
 function Event:remove(callback_object, callback_function_name)
 	if self:_is_call_reentrant() then
-		self:_add_during_reentrancy(callback_object, _callback_function_name)
+		self:_remove_during_reentrancy(callback_object, _callback_function_name)
 	else
-		self:_add(callback_object, callback_function_name)
+		self:_remove(callback_object, callback_function_name)
 	end
 end
 
@@ -119,24 +114,15 @@ function Event:_remove(callback_object, callback_function_name)
 	if obj_callbacks == nil then error() end
 	local callback_exists = obj_callbacks[callback_function_name] ~= nil
 
-	local obj_callbacks = self.callback_objects:get(callback_object)
+	if not callback_exists == true then error()	end
+	obj_callbacks[callback_function_name] = nil
 	
-	if obj_callbacks == nil then 
-		error()
+	self.callback_objects_num_entries[callback_object] = self.callback_objects_num_entries[callback_object] - 1
+	local num_entries = self.callback_objects_num_entries[callback_object]
+	if num_entries == 0 then
+		self.callback_objects[callback_object] = nil
+		self.callback_objects_num_entries[callback_object] = nil
 	end
-	
-	if obj_callbacks:get(callback_function_name) == nil then
-		error()
-	end
-
-	obj_callbacks = obj_callbacks:dissoc(callback_function_name)
-
-	if obj_callbacks:len() > 0 then
-		self.callback_objects = self.callback_objects:assoc(callback_object, obj_callbacks)
-	else
-		self.callback_objects = self.callback_objects:dissoc(callback_object)
-	end
-
 end
 
 function Event:_remove_during_reentrancy(callback_object, callback_function_name)
@@ -166,7 +152,9 @@ function Event:_is_call_reentrant()
 	return self.reentry_count > 0
 end
 
-function Event._is_func_callable(func)
+function Event._is_func_callable(callback_object, callback_function_name)
+	local func = callback_object[callback_function_name]
+	
 	if type(func) == "function" then
 		return true
 	end
